958465c30670b904510adadc4ed7c4ec762a2112
JLCardona <jcardona@cifpfbmoll.eu>
2021-10-22 20:07:32 +0200

    commit

--- app/src/main/java/com/example/task12a/MainActivity.java
+++ app/src/main/java/com/example/task12a/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.task12a;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,22 +10,34 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     int mCount = 0;
+    String KEY_COUNT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState != null){
+            mCount = savedInstanceState.getInt(KEY_COUNT);
+
+            TextView mShowCount = (TextView) findViewById(R.id.show_count);
+            mShowCount.setText(Integer.toString(mCount));
+        }
     }
 
     public void countUp(View view) {
-        TextView mShowCount = (TextView) findViewById(R.id.show_count);
         mCount++;
-        if (mShowCount != null)
+        TextView mShowCount = (TextView) findViewById(R.id.show_count);
             mShowCount.setText(Integer.toString(mCount));
     }
 
     public void showToast(View view) {
         Toast.makeText(this, Integer.toString(mCount), Toast.LENGTH_SHORT)
                 .show();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_COUNT, mCount);
     }
 }
